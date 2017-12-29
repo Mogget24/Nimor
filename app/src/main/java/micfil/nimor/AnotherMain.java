2d0c338deaad9d405d6e79aa34ecf1d41d27ed53
Mogget24 <metalmich24@gmail.com>
2017-12-29 14:47:33 +0100

    Added class

---
+++ app/src/main/java/micfil/nimor/AnotherMain.java
@@ -0,0 +1,8 @@
+package micfil.nimor;
+
+/**
+ * Created by michaelfilippo on 29/12/2017.
+ */
+
+public class AnotherMain {
+}
--- app/src/main/java/micfil/nimor/Main.java
+++ app/src/main/java/micfil/nimor/Main.java
@@ -9,5 +9,16 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int i = 1;
+
+
     }
+
+    private void something(){
+
+        int i = 2;
+
+    }
+
 }
