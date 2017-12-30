ea99871935321207ac1b697e1a4e7601fd95e039
Mogget24 <6150073+Mogget24@users.noreply.github.com>
2017-12-30 14:22:08 +0100

    White assets

--- app/src/main/java/micfil/nimor/Main.java
+++ app/src/main/java/micfil/nimor/Main.java
@@ -2,13 +2,19 @@ package micfil.nimor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class Main extends AppCompatActivity {
+
+    ImageView cover;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 
 }
