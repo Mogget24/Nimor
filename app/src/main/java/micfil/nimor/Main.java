fe19f9a3f5822d5ba15bfc152c55a6a1e58d635f
Mogget24 <metalmich24@gmail.com>
2018-01-05 17:23:45 +0100

    Seekbar 2

--- app/src/main/java/micfil/nimor/Main.java
+++ app/src/main/java/micfil/nimor/Main.java
@@ -1,44 +1,91 @@
 package micfil.nimor;
 
+import android.graphics.Typeface;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.opengl.Visibility;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class Main extends AppCompatActivity {
 
+    TextView title;
     ImageView cover;
     ImageButton play, pause, prev, next;
     MediaPlayer song;
+    Typeface MetalMacabre;
+    SeekBar seekBar;
+    Handler handler;
+    Runnable runnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Setup font
+        MetalMacabre = Typeface.createFromAsset(getAssets(),  "fonts/MetalMacabre.ttf");
+
+        // Setup controls
         play = findViewById(R.id.control_play);
         pause = findViewById(R.id.control_pause);
         prev = findViewById(R.id.control_prev);
         next = findViewById(R.id.control_next);
 
+        // Load custom song
         song = MediaPlayer.create(getApplicationContext(), R.raw.trivium);
+        song.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+        // Setup title
+        title = findViewById(R.id.song_title);
+        title.setTypeface(MetalMacabre);
+
+        // Seekbar
+
+        handler = new Handler();
+        seekBar = findViewById(R.id.seekbar);
+        seekBar.setMax(song.getDuration());
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean input) {
+                if(input){
+                    song.seekTo(progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
     }
 
-    // Play Musid
+    // Play Music
     public void Play(View v){
 
         play.setVisibility(View.GONE);
         pause.setVisibility(View.VISIBLE);
 
+        // Play song and handle seekbar
         song.start();
+        playCycle();
 
     }
 
-    // Pause Musid
+    // Pause Music
     public void Pause(View v){
 
         pause.setVisibility(View.GONE);
@@ -48,4 +95,25 @@ public class Main extends AppCompatActivity {
 
     }
 
+    // Seekbar handling
+    public void playCycle(){
+        seekBar.setProgress(song.getCurrentPosition());
+
+        if(song.isPlaying()){
+            runnable = new Runnable() {
+                @Override
+                public void run() {
+                    playCycle();
+                }
+            };
+            handler.postDelayed(runnable, 1000);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        song.release();
+        handler.removeCallbacks(runnable);
+    }
 }
